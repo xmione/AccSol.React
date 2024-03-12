0ebe8a127b9e8ac0adf91ae5319b788d372f0bff
Solomio S. Sisante <solomiosisante@gmail.com>
2024-03-12 23:39:37 +0800

    set https server in the swagger2.json file and specified http client base address in Program.cs file.

--- AccSol.React/Program.cs
+++ AccSol.React/Program.cs
@@ -9,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var _baseAPIUrl = builder.Configuration.GetSection("APIBaseURL").Value ?? string.Empty;
 
+builder.Services.AddHttpClient<AccSolService>().ConfigureHttpClient((sp, client) =>
+{
+    client.BaseAddress = new Uri("https://localhost:7040/");
+});
+
 builder.Services.AddHttpClient<SpotifyService>();
 builder.Services.AddHttpClient<AccSolService>();
 builder.Services
@@ -17,7 +22,7 @@ builder.Services
     //.AddQueryType<AppQuery>()
     //.AddType<EmployeeType>()
     //.AddType<DepartmentType>()
-    .AddQueryType<AppSchema>()
+    //.AddQueryType<AppSchema>()
     .RegisterService<SpotifyService>()
     .RegisterService<AccSolService>();
 
